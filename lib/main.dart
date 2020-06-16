6a1b68eae2d3a4395837e7ad359d5235be3617db
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-06-17 01:04:54 +0900

    チュートリアル Step3まで
    https://flutter.dev/docs/get-started/codelab#step-3-add-a-stateful-widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
         title: 'Welcome to Flutter',
         home: Scaffold(
           appBar: AppBar(
             title: Text('Welcome to Flutter'),
           ),
-          body: Center(
-            child: Text(wordPair.asPascalCase),
-          ),
+          body: Center(child: RandomWord()),
         ));
   }
+}
+
+class RandomWordState extends State<RandomWord> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  @override
+  RandomWordState createState() => RandomWordState();
 }
